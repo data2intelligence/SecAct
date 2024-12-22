ba5b7485bba92c8f2a0da8b37453a68a2b2ef599
beibeiru <33799867+beibeiru@users.noreply.github.com>
2024-12-22 10:21:39 -0500

    comp

--- R/activity.R
+++ R/activity.R
@@ -1,4 +1,76 @@
 #' @title Secreted protein activity inference
+#' @description Infer the activity of over 1000 secreted proteins from tumor gene expression profiles.
+#' @param expr Gene expression matrix with gene symbol (row) x sample (column).
+#' @param sigMatrix Secreted protein signature matrix.
+#' @param lambda Penalty factor in the ridge regression.
+#' @param nrand Number of randomizations in the permutation test, with a default value 1000.
+#' @return
+#'
+#' A list with four items, each is a matrix.
+#' beta: regression coefficients
+#' se: standard errors of coefficients
+#' zscore: beta/se
+#' pvalue: statistical significance
+#'
+#' @examples
+#'
+#' exprPath <- file.path(system.file(package = "SecAct"), "extdata/IFNG_GSE100093.diff")
+#' expr <- read.table(expr, sep="\t", check.names=F)
+#' res <- SecAct.inference(expr, lambda=10000, nrand=1000)
+#' head(res$zscore)
+#'
+#' @rdname SecAct.inference
+#' @export
+#'
+SecAct.inference <- function(expr, SigMat="SecAct", lambda=5e+5, nrand=1000)
+{
+    if(SigMat=="SecAct")
+    {
+      Xfile<- file.path(system.file(package = "SecAct"), "extdata/AllSigFilteredBy_MoranI_TCGA_ICGC_0.25_ds3.tsv")
+      X <- read.table(Xfile,sep="\t",check.names=F)
+    }else{
+      X <- read.table(SigMat,sep="\t",check.names=F)
+    }
+
+    Y <- expr
+
+    olp <- intersect(row.names(Y),row.names(X))
+    X <- as.matrix(X[olp,,drop=F])
+    Y <- as.matrix(Y[olp,,drop=F])
+
+    X <- scale(X)
+    Y <- scale(Y)
+
+    n <- length(olp)
+    p <- ncol(X)
+    m <- ncol(Y)
+
+    res <- .C("ridgeReg",
+              X=as.double(t(X)),
+              Y=as.double(t(Y)),
+              as.integer(n),
+              as.integer(p),
+              as.integer(m),
+              as.double(lambda),
+              as.double(nrand),
+              beta=double(p*m),
+              se=double(p*m),
+              zscore=double(p*m),
+              pvalue=double(p*m)
+    )
+
+    beta <- matrix(res$beta,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
+    se <- matrix(res$se,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
+    zscore <- matrix(res$zscore,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
+    pvalue <- matrix(res$pvalue,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
+
+    res <- list(beta=beta, se=se, zscore=zscore, pvalue=pvalue)
+
+    res
+}
+
+
+#' @title Secreted protein activity inference
 #' @description Infer the signaling activity of 1248 secreted proteins from gene expression profiles.
 #' @param inputProfile Gene expression matrix with gene symbol (row) x sample (column).
 #' @param is.differential A logical indicating whether inputProfile has been differential profiles against to control.
